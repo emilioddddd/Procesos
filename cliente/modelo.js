2a7c1dea17d2f50c9ae6e24cc811bedbb271a898
emilioddddd <egalindofelipe@gmail.com>
2025-05-31 18:31:55 +0200

    1.1Implementar la lógica y las pruebas

---
+++ cliente/modelo.js
@@ -0,0 +1,49 @@
+function Sistema() {
+    this.usuarios = {};
+
+    //Coleccion de funciones que permiten gestionar los usuarios
+    this.agregarUsuario = function(nick){
+        let res={"nick":-1};
+        if (!this.usuarios[nick]){
+            this.usuarios[nick]=new Usuario(nick);
+            res.nick=nick;
+        }
+        else{
+            console.log("el nick "+nick+" está en uso"); 
+        }
+        return res;
+    }
+
+    this.eliminarUsuario = function(nick){
+        res = {"eliminado":false}
+        if(this.usuarios[nick]){
+            delete this.usuarios[nick]
+            res = {"eliminado":true}
+        }
+        return res
+    }
+
+    this.obtenerUsuarios = function(){
+        let res = {"list":[]};
+        for (let i=0; i < Object.keys(this.usuarios).length; i++){
+            res.list.push(Object.keys(this.usuarios)[i]);
+        }
+        return res
+    }
+
+    this.usuarioActivo = function(nick){
+        let res = {"activo":false}
+        if (this.usuarios[nick]!=undefined){
+            return res = {"activo":true};
+        }
+        return res
+    }
+    this.numeroUsuarios = function(){
+        //return Object.keys(this.usuarios).length;
+        let res = {"num":Object.keys(this.usuarios).length};
+        return res
+    }
+}
+function Usuario(nick) {
+    this.nick = nick;
+} 
---
+++ jasmine-standalone-5.3.0/spec/modeloSpec.js
@@ -0,0 +1,63 @@
+describe('El sistema', function() { 
+  let sistema; 
+  beforeEach(function() { 
+    sistema=new Sistema() 
+  }); 
+
+  it('inicialmente no hay usuarios', function() { 
+    expect(sistema.numeroUsuarios().num).toBe(0); 
+  }); 
+  it("Comprobamos agregar usuario con nick", function(){
+    //comprobar que no hay usuarios
+    expect(sistema.numeroUsuarios().num).toEqual(0);
+    //agregar un usuario concreto
+    sistema.agregarUsuario("Pepe");
+    //comprobar que hay 1 usuario en el sistema
+    expect(sistema.numeroUsuarios().num).toEqual(1);
+    expect(sistema.usuarioActivo("Pepe").activo).toEqual(true);
+
+  });
+
+  it("Comprobamos eliminar usuario", function(){
+    //comprobar que no hay usuario
+    expect(sistema.numeroUsuarios().num).toEqual(0);
+    //agregamos un usuario
+    sistema.agregarUsuario("Pepe");
+    //comprobamos que el usuario creado esta en el sistema
+    expect(sistema.numeroUsuarios().num).toEqual(1);
+    expect(sistema.usuarioActivo("Pepe").activo).toEqual(true);
+    //eliminamos el usuario
+    sistema.eliminarUsuario("Pepe");
+    //comprobamos que el usuario eliminado no esta en el sistema
+    expect(sistema.numeroUsuarios().num).toEqual(0);
+  });
+
+  it("comprobar usuario activo", function(){
+    //comprobar que no hay usuario
+    expect(sistema.numeroUsuarios().num).toEqual(0);
+    //agregamos un usuario
+    sistema.agregarUsuario("Pepe");
+    //comprobamos que el usuario creado esta en el sistema
+    expect(sistema.numeroUsuarios().num).toEqual(1);
+    expect(sistema.usuarioActivo("Pepe").activo).toEqual(true);
+  });
+
+  it("comprobar el método número de usuarios", function(){
+    //calcular las claves del array asociativo Object.keys(sistema.usuarios)
+    //comprobamos que el valor pro numeroUsuarios() es igual al anterior
+    let res=sistema.numeroUsuarios();
+    expect(res.num).toEqual(0);
+    sistema.agregarUsuario("Pepe");
+    res=sistema.numeroUsuarios();
+    expect(res.num).toEqual(1);
+  });
+
+  it("comprobar el metodo obtener usuarios", function(){
+    let res = sistema.obtenerUsuarios();
+    expect(res.list).toEqual([]);
+    sistema.agregarUsuario("Pepe");
+    sistema.agregarUsuario("Luis");
+    res = sistema.obtenerUsuarios();
+    expect(res.list).toEqual(["Pepe", "Luis"])
+  })
+}) 
